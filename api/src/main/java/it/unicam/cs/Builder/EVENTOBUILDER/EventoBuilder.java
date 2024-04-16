58bc33908d04f4aa34669bce33a1be90af08ca60
nicio2023 <nicolas.rossi@studenti.unicam.it>
2024-04-17 00:05:35 +0200

    builder per evento

--- api/src/main/java/it/unicam/cs/Builder/EVENTOBUILDER/EventoBuilder.java
+++ api/src/main/java/it/unicam/cs/Builder/EVENTOBUILDER/EventoBuilder.java
@@ -1,4 +1,60 @@
 package it.unicam.cs.Builder.EVENTOBUILDER;
 
-public class EventoBuilder {
+import it.unicam.cs.model.Comune;
+import it.unicam.cs.model.Utente;
+import it.unicam.cs.model.abstractions.Evento;
+import it.unicam.cs.model.abstractions.POI;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+@Data
+@AllArgsConstructor @NoArgsConstructor
+public abstract class EventoBuilder {
+    private Integer id;
+    private Comune comuneAssociato;
+    private String nome;
+    private String descrizione;
+    private Utente contributore;
+    private POI poiAssociato;
+    private LocalDateTime dataInizio;
+    private LocalDateTime dataFine;
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setComuneAssociato(Comune comuneAssociato) {
+        this.comuneAssociato = comuneAssociato;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public void setContributore(Utente contributore) {
+        this.contributore = contributore;
+    }
+
+    public void setPoiAssociato(POI poiAssociato) {
+        this.poiAssociato = poiAssociato;
+    }
+
+    public void setDataInizio(LocalDateTime dataInizio) {
+        this.dataInizio = dataInizio;
+    }
+
+    public void setDataFine(LocalDateTime dataFine) {
+        this.dataFine = dataFine;
+    }
+    public abstract Evento build();
 }
